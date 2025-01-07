0ba26aa4e2e6098292f3113580f98315e2fcaa93
Adiazgomez68 <adiazgomez68@correo.unicordoba.edu.co>
2025-01-07 15:55:11 -0500

    Fixed media icons color

--- core/components/shared/footer/Footer.jsx
+++ core/components/shared/footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
     <section id="footer" className="w-full h-full">
       <Wrapper className="flex-col py-10 text-primary-dark dark:text-secondary space-y-5 xl:space-y-0">
         <div className="space-x-5 justify-center items-center flex xl:hidden">
-          <LinkedIn className="text-primary-dark duration-300 hover:text-secondary hover:-translate-y-1" />
-          <Github className="text-primary-dark duration-300 hover:text-secondary hover:-translate-y-1" />
+          <LinkedIn className="text-primary-dark duration-300 dark:hover:text-secondary dark:text-primary hover:text-secondary hover:-translate-y-1" />
+          <Github className="text-primary-dark duration-300 dark:text-primary dark:hover:text-secondary hover:text-secondary hover:-translate-y-1" />
         </div>
         <p className="w-full text-center font-inconsolada text-primary-dark dark:text-primary text-xs md:text-sm">
           Diseñado y construido por Andrés Díaz
