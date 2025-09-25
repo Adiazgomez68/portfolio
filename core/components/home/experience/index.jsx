e6f7bbf02c26d39116c98e1cf362bffcc75a0e73
Adiazgomez68 <adiazgomez68@correo.unicordoba.edu.co>
2025-09-25 10:56:45 -0500

    Updated footer structure and social media

--- core/components/home/experience/index.jsx
+++ core/components/home/experience/index.jsx
@@ -10,7 +10,7 @@ const Experience = () => {
   const { theme } = useTheme();
 
   return (
-    <section id="experience" className="w-full z-[50] dark:bg-[#12181d]">
+    <section id="experience" className="w-full">
       <Wrapper className="flex-col mb-6 mt-10 px-5 md:mt-6 lg:mt-14 items-center justify-center">
         <h2 className="text-3xl font-semibold text-center mb-10 glow-text">
           Experiencia Profesional
--- core/components/shared/Media.jsx
+++ core/components/shared/Media.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import { Download, Email, Github, LinkedIn } from "./icons/SolciaMedias";
 import Wrapper from "./Wrapper";
+import { twMerge } from "tailwind-merge";
 
-const Media = ({ children }) => {
+const Media = () => {
+  const [styles, setStyles] = useState();
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const handleScroll = () => {
+      const shouldAnimate = window.scrollY >= 50;
+
+      if (shouldAnimate) {
+        setStyles("animate-fade-down");
+      } else {
+        setStyles("animate-fade-up");
+      }
+    };
+
+    handleScroll();
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <Wrapper
-      className="hidden xl:flex xl:flex-col z-[99] xl:py-10 xl:text-primary xl:dark:text-secondary"
+      className="hidden xl:flex xl:flex-col dark:bg-[#12181d] z-[99] xl:text-primary xl:dark:text-secondary"
       maxScreen="max-w-screen-xl"
     >
-      <div className="flex flex-col items-center space-y-6 fixed left-28 bottom-0 animate-fade-up">
+      <div
+        className={twMerge("flex flex-col items-center space-y-6 fixed left-28 bottom-0", styles)}
+      >
         <div className="space-y-5 flex-col flex">
           <LinkedIn className="text-secondary dark:text-secondary duration-300 dark:hover:text-primary hover:-translate-y-1" />
           <Github className="text-secondary dark:text-secondary duration-300 dark:hover:text-primary hover:-translate-y-1" />
@@ -16,7 +40,9 @@ const Media = ({ children }) => {
         <div className="w-[1px] bg-secondary dark:bg-white h-40"></div>
       </div>
 
-      <div className="flex flex-col items-center space-y-6 fixed right-28 bottom-0 animate-fade-up">
+      <div
+        className={twMerge("flex flex-col items-center space-y-6 fixed right-28 bottom-0", styles)}
+      >
         <Download className="text-secondary dark:text-secondary duration-300 dark:hover:text-primary hover:-translate-y-1" />
         <div className="w-[1px] bg-secondary dark:bg-white h-40"></div>
       </div>
